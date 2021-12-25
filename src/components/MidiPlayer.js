304c7ed300eb097ee5c0a5daecff61622c3e88a1
YMGYM <good_day1234@naver.com>
2021-12-25 22:46:35 +0900

    [INIT] 컴포넌트 추가

---
+++ src/components/MidiPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Midiplayer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Midiplayer;
---
+++ src/components/credit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Credit.css';
+
+const Credit = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Credit;
