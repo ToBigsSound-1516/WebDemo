1d09edbe16de395d380f2f7e5d2d1316cd47f262
YMGYM <good_day1234@naver.com>
2021-12-26 23:15:41 +0900

    [FEATURE] 테스트 미디 플레이어 임포트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Midiplayer from './components/MidiPlayer.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Midiplayer></Midiplayer>
     </div>
   );
 }
--- src/components/MidiPlayer.js
+++ src/components/MidiPlayer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import 'html-midi-player';
 
 const Midiplayer = () => {
     return (
         <div>
-            
+            <h1>안녕안녕 나는 지수야</h1>
+
+            <script src="https://cdn.jsdelivr.net/combine/npm/tone@14.7.58,npm/@magenta/music@1.23.1/es6/core.js,npm/focus-visible@5,npm/html-midi-player@1.4.0"></script>
+
+            <midi-player src="https://magenta.github.io/magenta-js/music/demos/melody.mid" sound-font visualizer="#myVisualizer"></midi-player>
+            <midi-visualizer type="piano-roll" id="myVisualizer"></midi-visualizer>
         </div>
     );
 }
