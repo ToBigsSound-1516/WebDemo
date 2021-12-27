959153ceb7cf69bc45601c2f2e821a7c983a99fc
YMGYM <good_day1234@naver.com>
2021-12-27 23:34:37 +0900

    [FEATURE] change midi module

--- src/components/MidiPlayer.js
+++ src/components/MidiPlayer.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import 'html-midi-player';
+import PianoRoll from 'react-piano-roll';
 
 const Midiplayer = () => {
     return (
         <div>
             <h1>안녕안녕 나는 지수야</h1>
 
-            <script src="https://cdn.jsdelivr.net/combine/npm/tone@14.7.58,npm/@magenta/music@1.23.1/es6/core.js,npm/focus-visible@5,npm/html-midi-player@1.4.0"></script>
-
-            <midi-player src="https://magenta.github.io/magenta-js/music/demos/melody.mid" sound-font visualizer="#myVisualizer"></midi-player>
-            <midi-visualizer type="piano-roll" id="myVisualizer"></midi-visualizer>
+            <PianoRoll
+            width={1200}
+            height={660}
+            zoom={6}
+            resolution={2}
+            gridLineColor={0x333333}
+            blackGridBgColor={0x1e1e1e}
+            whiteGridBgColor={0x282828}
+            noteData={[
+                ["0:0:0", "F5", ""],
+                ["0:0:0", "C4", "2n"],
+                ["0:0:0", "D4", "2n"],
+                ["0:0:0", "E4", "2n"],
+                ["0:2:0", "B4", "4n"],
+                ["0:3:0", "A#4", "4n"],
+                ["0:0:0", "F2", ""],
+            ]}
+            />;
+          
         </div>
     );
 }
