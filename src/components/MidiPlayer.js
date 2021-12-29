df8fda879109564e6c663a67d0169be4b66763ba
YMGYM <good_day1234@naver.com>
2021-12-29 23:03:35 +0900

    [FEATURE] 미디 플레이어 UI 설정1

--- src/components/MidiPlayer.js
+++ src/components/MidiPlayer.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import {Container, Row, Col, Button, Form} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MidiPlayer.css';
 
 const Midiplayer = () => {
     return (
-        <div>
-            <h1>안녕안녕 나는 지수야</h1>
+        <div className="background" >
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <h1>
+                            투빅스 턴테이블
+                        </h1>
+                    </Col>
+                </Row>
 
+                <Row className="justify-content-center">
+                    <Col md={6} ><div className="Track" id="Track1"></div></Col>
+                    <Col md={6} ><div className="Track" id="Track2"></div></Col>
+                </Row>
+
+                <Row className="justify-content-center">
+                    <Col md={1}><Form.Control type="number" placeholder="Enter email" /></Col>
+                    <Col md={5}><Form.Label>Range1</Form.Label><Form.Range value={30} /></Col>
+                    <Col md={1}><Form.Control type="number" placeholder="Enter email" /></Col>
+                    <Col md={5}><Form.Label>Range2</Form.Label><Form.Range value={100} /></Col>
+                </Row>
+
+                <Row>
+                    <Button as={Col} variant="primary">Button #1</Button>
+                </Row>
+
+
+            </Container>
         </div>
     );
 }
