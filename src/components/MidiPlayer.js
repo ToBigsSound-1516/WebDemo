b9149fe451507fb8050d7b4a31196b60af6bd329
YMGYM <good_day1234@naver.com>
2021-12-30 23:45:12 +0900

    [FEATURE] 레인지 바 입력

--- src/components/MidiPlayer.js
+++ src/components/MidiPlayer.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import {Container, Row, Col, Button, Form} from 'react-bootstrap';
+import { Range } from 'react-range';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MidiPlayer.css';
 
 const Midiplayer = () => {
+    var state = { values: [50] };
+
+    var renderTrack = () => {
+        <div
+          style={{
+            height: '6px',
+            width: '100%',
+            backgroundColor: '#ccc'
+          }}
+        >
+        </div>
+    };
+
+    var renderThumb= () => {
+        <div
+          style={{
+            height: '42px',
+            width: '42px',
+            backgroundColor: '#999'
+          }}
+        />
+    };
+
     return (
         <div className="background" >
             <Container fluid>
@@ -22,17 +46,40 @@ const Midiplayer = () => {
                 </Row>
 
                 <Row className="justify-content-center">
+                    {/* <Col md={1}><Form.Control type="number" placeholder="Enter email" /></Col>
+                    <Col md={5}><Range step={0.1} min={0} max={100} /></Col>
                     <Col md={1}><Form.Control type="number" placeholder="Enter email" /></Col>
-                    <Col md={5}><Form.Label>Range1</Form.Label><Form.Range value={30} /></Col>
-                    <Col md={1}><Form.Control type="number" placeholder="Enter email" /></Col>
-                    <Col md={5}><Form.Label>Range2</Form.Label><Form.Range value={100} /></Col>
+                    <Col md={5}><Range step={0.1} min={0} max={100} /></Col> */}
                 </Row>
 
                 <Row>
                     <Button as={Col} variant="primary">Button #1</Button>
                 </Row>
-
-
+                
+                <Range values={state.values} renderTrack={({ props, children }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '6px',
+              width: '100%',
+              backgroundColor: '#ccc'
+            }}
+          >
+            {children}
+          </div>
+        )}
+        renderThumb={({ props }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '42px',
+              width: '42px',
+              backgroundColor: '#999'
+            }}
+          />
+        )} />
             </Container>
         </div>
     );
