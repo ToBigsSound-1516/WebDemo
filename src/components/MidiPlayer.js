ee414aa273f08d2054855d60abc516be05394bdb
YMGYM <good_day1234@naver.com>
2021-12-31 21:30:29 +0900

    [CHANGE] 미디 플레이어 레이아웃 변경

--- src/components/MidiPlayer.js
+++ src/components/MidiPlayer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import {Container, Row, Col, Button, Form} from 'react-bootstrap';
 import { Range } from 'react-range';
 
@@ -6,80 +6,97 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './MidiPlayer.css';
 
 const Midiplayer = () => {
-    var state = { values: [50] };
+    const [aValues, setAValues] = useState([50]);
+    const [bValues, setBValues] = useState([30]);
 
-    var renderTrack = () => {
-        <div
-          style={{
-            height: '6px',
-            width: '100%',
-            backgroundColor: '#ccc'
-          }}
-        >
-        </div>
-    };
+    var renderRange = (start, end, initValue, stateCallback) => {
+      return(
+        <Range
+          step={1}
+          min={start}
+          max={end}
 
-    var renderThumb= () => {
-        <div
-          style={{
-            height: '42px',
-            width: '42px',
-            backgroundColor: '#999'
-          }}
+          values={initValue}
+          onChange={(values) => stateCallback(values)}
+          renderTrack={({ props, children }) => (
+            <div
+              {...props}
+              style={{
+                ...props.style,
+                height: '6px',
+                width: '100%',
+                backgroundColor: '#D1F2EB'
+              }}
+            >
+              {children}
+            </div>
+          )}
+          renderThumb={({ props }) => (
+            <div
+              {...props}
+              style={{
+                ...props.style,
+                height: '42px',
+                width: '42px',
+                backgroundColor: '#1C2833',
+                border: '3px solid',
+                borderColor: '#D1F2EB'
+              }}
+            />
+          )}
         />
-    };
-
+      )
+    }
     return (
         <div className="background" >
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <h1>
-                            투빅스 턴테이블
-                        </h1>
-                    </Col>
+            <Container>
+                <Row className="marginTop3 justify-content-center">
+                  <Col md={6}>
+                    <Form.Select size="lg">
+                      <option value={"Combination 1"}>Don't Look Back In Anger + Isn't She Lovely</option>
+                      <option value={"Combination 2"}>샘플 곡 조합2</option>
+                      <option value={"Combination 3"}>샘플 곡 조합3</option>
+                    </Form.Select>
+                  </Col>
                 </Row>
 
-                <Row className="justify-content-center">
-                    <Col md={6} ><div className="Track" id="Track1"></div></Col>
-                    <Col md={6} ><div className="Track" id="Track2"></div></Col>
+                <Row className="marginTop3 justify-content-center">
+                  <Col className="justify-content-center" md={3}>
+                    <Form.Select value={"Song2"}>
+                      <option value={"Song1"}>Isn't She Lovely</option>
+                      <option value={"Song2"}>Don't Look Back In Anger</option>
+                    </Form.Select>
+                  </Col>
+
+                  <Col md={3}></Col>
+
+                  <Col className="justify-content-center" md={3}>
+                    <Form.Select>
+                      <option value={"Song1"}>Isn't She Lovely</option>
+                      <option value={"Song2"}>Don't Look Back In Anger</option>
+                    </Form.Select>
+                  </Col>
                 </Row>
 
-                <Row className="justify-content-center">
-                    {/* <Col md={1}><Form.Control type="number" placeholder="Enter email" /></Col>
-                    <Col md={5}><Range step={0.1} min={0} max={100} /></Col>
-                    <Col md={1}><Form.Control type="number" placeholder="Enter email" /></Col>
-                    <Col md={5}><Range step={0.1} min={0} max={100} /></Col> */}
+                <Row className="marginTop5 justify-content-center">
+                    <Col md={6} ><div className="Track Album1" id="Track1"></div></Col>
+                    <Col md={6} ><div className="Track Album2" id="Track2"></div></Col>
                 </Row>
 
-                <Row>
-                    <Button as={Col} variant="primary">Button #1</Button>
+
+                <Row className="marginTop5">
+                  <Col md={1}></Col>
+                  <Col md={4}>{renderRange(0,100, aValues, setAValues)}</Col>
+                  <Col md={2}></Col>
+                  <Col md={4}>{renderRange(0,100, bValues, setBValues)}</Col>
+                  <Col md={1}></Col>
+                </Row>
+
+                <Row className="marginTop5 justify-content-center">
+                    <Button md={3} as={Col} variant="secondary">Play Song!</Button>
                 </Row>
                 
-                <Range values={state.values} renderTrack={({ props, children }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: '6px',
-              width: '100%',
-              backgroundColor: '#ccc'
-            }}
-          >
-            {children}
-          </div>
-        )}
-        renderThumb={({ props }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: '42px',
-              width: '42px',
-              backgroundColor: '#999'
-            }}
-          />
-        )} />
+                
             </Container>
         </div>
     );
