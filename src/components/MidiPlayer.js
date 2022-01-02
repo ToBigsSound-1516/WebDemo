89e4fffec8c97201a7fd1c8eb651a2de1c2d7243
YMGYM <good_day1234@naver.com>
2022-01-02 20:43:43 +0900

    [CHANGE] MIDI 불러오기 변경

--- src/components/MidiPlayer.js
+++ src/components/MidiPlayer.js
@@ -9,10 +9,19 @@ import './MidiPlayer.css';
 
 const Midiplayer = () => {
     // state 설정
-    const [aValues, setAValues] = useState([50]); // 왼쪽 곡 마디 틱
-    const [bValues, setBValues] = useState([30]); // 오른쪽 곡 마디 틱
-    const [midFile, setMidFile] = useState("https://magenta.github.io/magenta-js/music/demos/melody.mid");
+    const [aValues, setAValues] = useState([50]); // 왼쪽 곡 마디 틱 (Range에서 사용)
+    const [bValues, setBValues] = useState([30]); // 오른쪽 곡 마디 틱 (Range에서 사용)
 
+    const [aSong, setASong] = useState("Album1"); // 왼쪽 곡 종류 (Select에서 사용)
+    const [bSong, setBSong] = useState("Album2"); // 오른쪽 곡 종류 (Select에서 사용)
+    const [mixSong, setMixSong] = useState("Combination 1"); // 조합 곡 종류 (Select에서 사용)
+    const [aArt, setAArt] = useState("Track Album1"); // 왼쪽 곡에 적용될 앨범아트
+    const [bArt, setBArt] = useState("Track Album2"); // 왼쪽 곡에 적용될 앨범아트
+    const [midFile, setMidFile] = useState("midiFiles/DontLookBackinAnger.mid");
+
+
+    
+    // 노래의 범위를 설정하는 함수 stateCallback 파라미터는 함수를 인자로 받아 어떤 스테이트를 변경할지 설정함
     var renderRange = (start, end, initValue, stateCallback) => {
       return(
         <Range
@@ -50,13 +59,28 @@ const Midiplayer = () => {
           )}
         />
       )
-    }
+    };    
+
+    var handleChangeSong = (value, trackNumber) => { // 트랙의 번호를 입력받아서 해당 트랙의 앨범과 폼 표시 내용까지 모두 바꿉니다.
+      if (trackNumber == 1) {
+        console.log(value);
+        setASong(value);
+        setAArt(`Track ${value}`);
+      }
+      else{
+        setBSong(value);
+        setBArt(`Track ${value}`);
+      }
+      
+
+    };
+
     return (
         <div className="background" >
             <Container>
                 <Row className="marginTop3 justify-content-center">
                   <Col md={6}>
-                    <Form.Select size="lg">
+                    <Form.Select size="lg" onChange={(e) => {setMixSong(e.target.value)}}>
                       <option value={"Combination 1"}>Don't Look Back In Anger + Isn't She Lovely</option>
                       <option value={"Combination 2"}>샘플 곡 조합2</option>
                       <option value={"Combination 3"}>샘플 곡 조합3</option>
@@ -66,25 +90,26 @@ const Midiplayer = () => {
 
                 <Row className="marginTop3 justify-content-center">
                   <Col className="justify-content-center" md={3}>
-                    <Form.Select value={"Song2"}>
-                      <option value={"Song1"}>Isn't She Lovely</option>
-                      <option value={"Song2"}>Don't Look Back In Anger</option>
+                    <Form.Select value={aSong} onChange={(e) => {handleChangeSong(e.target.value, 1)}}>
+                      <option value={"Album1"}>Isn't She Lovely</option>
+                      <option value={"Album2"}>Don't Look Back In Anger</option>
                     </Form.Select>
                   </Col>
 
                   <Col md={3}></Col>
 
                   <Col className="justify-content-center" md={3}>
-                    <Form.Select>
-                      <option value={"Song1"}>Isn't She Lovely</option>
-                      <option value={"Song2"}>Don't Look Back In Anger</option>
+                    <Form.Select value={bSong} onChange={(e) => {handleChangeSong(e.target.value, 2)}}>
+                      <option value={"Album1"}>Isn't She Lovely</option>
+                      <option value={"Album2"}>Don't Look Back In Anger</option>
                     </Form.Select>
                   </Col>
                 </Row>
 
+                // 앨벙아트 설정
                 <Row className="marginTop5 justify-content-center">
-                    <Col md={6} ><div className="Track Album1" id="Track1"></div></Col>
-                    <Col md={6} ><div className="Track Album2" id="Track2"></div></Col>
+                    <Col md={6} ><div className={aArt} id="Track1"></div></Col>
+                    <Col md={6} ><div className={bArt} id="Track2"></div></Col>
                 </Row>
 
 
